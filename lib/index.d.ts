33169f60220d69952b801a3b15f54d30442dc5ce
Mateusz <mateusz@qunabu.com>
2021-09-30 08:51:07 +0200

    h5p player veriosn upate

--- lib/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { default as Image } from "./components/Image";
-import * as API from "./types/api";
-export { EscolaLMSContextProvider, SCORMPlayer, EscolaLMSContext, sortProgram, } from "./context/index";
-export { useLocalStorage } from "./hooks/useLocalStorage";
-export { API };
--- lib/services/pages.d.ts
+++
@@ -1,9 +0,0 @@
-import * as API from "../types/api";
-/**  GET /api/pages */
-export declare function pages(options?: {
-    [key: string]: any;
-}): Promise<API.PageList>;
-/**  GET /api/pages/:slug */
-export declare function page(slug: string, options?: {
-    [key: string]: any;
-}): Promise<API.DefaultResponse<API.Page>>;
