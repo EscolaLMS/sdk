9f815a28efd2deaf23597dc15a5d472c662be2ba
Przemek <przemek.wiktorski@escola.pl>
2021-12-02 16:27:29 +0100

    changed topic types

--- src/react/context/index.tsx
+++ src/react/context/index.tsx
@@ -620,6 +620,7 @@ export const EscolaLMSContextProvider: FunctionComponent<IMock> = ({
       loading: false,
     });
     setCart(defaultConfig.cart);
+    // setProgress([]);
     return Promise.resolve();
   }, []);
 
