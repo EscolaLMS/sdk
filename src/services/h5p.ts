4079d76ad592a9b6e6c9e866d240ad228226662c
Przemek <przemek.wiktorski@escola.pl>
2022-01-24 09:12:42 +0100

    h5p update

---
+++ src/services/h5p.ts
@@ -0,0 +1,13 @@
+import request from "umi-request";
+import * as API from "../types/api";
+
+export async function getH5p(id: number, options?: { [key: string]: any }) {
+  return request<API.DefaultResponse<API.H5PObject>>(
+    `/api/hh5p/content/${id}`,
+    {
+      method: "GET",
+      /* useCache: true */ useCache: false,
+      ...(options || {}),
+    }
+  );
+}
