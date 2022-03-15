365be8cab961bb3e8659d5fbcb875c378adcb202
Mateusz Motyka <mati@mateuszs-mini.home>
2022-03-15 17:40:42 +0100

    remove duplicated function

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,11 +1,14 @@
-import request from 'umi-request';
-import * as API from '../types/api';
+import request from "umi-request";
+import * as API from "../types/api";
 
-export async function login(body: API.LoginRequest, options?: { [key: string]: any }) {
-  return request<API.LoginResponse>('/api/auth/login', {
-    method: 'POST',
+export async function login(
+  body: API.LoginRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.LoginResponse>("/api/auth/login", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     data: body,
     ...(options || {}),
@@ -13,34 +16,40 @@ export async function login(body: API.LoginRequest, options?: { [key: string]: a
 }
 
 export async function profile(token: string) {
-  return request<API.DefaultResponse<API.UserItem>>('/api/profile/me', {
-    method: 'GET',
+  return request<API.DefaultResponse<API.UserItem>>("/api/profile/me", {
+    method: "GET",
     headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
+      Accept: "application/json",
+      "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
     },
   });
 }
 
-export async function register(body: API.RegisterRequest, options?: { [key: string]: any }) {
-  return request<API.DefaultResponse<API.RegisterResponse>>('/api/auth/register', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    data: body,
-    ...(options || {}),
-  });
+export async function register(
+  body: API.RegisterRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.DefaultResponse<API.RegisterResponse>>(
+    "/api/auth/register",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      data: body,
+      ...(options || {}),
+    }
+  );
 }
 
 export async function updateProfile(body: API.UserItem, token: string) {
-  return request<API.DefaultResponse<API.UserItem>>('/api/profile/me', {
-    method: 'PUT',
+  return request<API.DefaultResponse<API.UserItem>>("/api/profile/me", {
+    method: "PUT",
     data: body,
     headers: {
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
+      Accept: "application/json",
+      "Content-Type": "application/json",
       Authorization: `Bearer ${token}`,
     },
   });
@@ -48,33 +57,42 @@ export async function updateProfile(body: API.UserItem, token: string) {
 
 export async function updateAvatar(file: File, token: string) {
   const formData = new FormData();
-  formData.append('avatar', file);
-  return request<API.DefaultResponse<API.UserItem>>('/api/profile/upload-avatar', {
-    method: 'POST',
-    data: formData,
-    headers: {
-      Accept: 'application/json',
-      Authorization: `Bearer ${token}`,
-    },
-  });
+  formData.append("avatar", file);
+  return request<API.DefaultResponse<API.UserItem>>(
+    "/api/profile/upload-avatar",
+    {
+      method: "POST",
+      data: formData,
+      headers: {
+        Accept: "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
 }
 
-export async function forgot(body: API.ForgotRequest, options?: { [key: string]: any }) {
-  return request<API.AuthResponse>('/api/auth/password/forgot', {
-    method: 'POST',
+export async function forgot(
+  body: API.ForgotRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.AuthResponse>("/api/auth/password/forgot", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     data: body,
     ...(options || {}),
   });
 }
 
-export async function reset(body: API.ResetPasswordRequest, options?: { [key: string]: any }) {
-  return request<API.AuthResponse>('/api/auth/password/reset', {
-    method: 'POST',
+export async function reset(
+  body: API.ResetPasswordRequest,
+  options?: { [key: string]: any }
+) {
+  return request<API.AuthResponse>("/api/auth/password/reset", {
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
     data: body,
     ...(options || {}),
@@ -83,18 +101,9 @@ export async function reset(body: API.ResetPasswordRequest, options?: { [key: st
 
 export async function emailVerify(id: string, hash: string) {
   return request<API.AuthResponse>(`/api/auth/email/verify/${id}/${hash}`, {
-    method: 'GET',
+    method: "GET",
     headers: {
-      'Content-Type': 'application/json',
-    },
-  });
-}
-export async function refreshToken(token: string) {
-  return request<API.DefaultResponse<{ token: string; expires_at: string }>>('/api/auth/refresh', {
-    method: 'GET',
-    headers: {
-      Accept: 'application/json',
-      Authorization: `Bearer ${token}`,
+      "Content-Type": "application/json",
     },
   });
 }
