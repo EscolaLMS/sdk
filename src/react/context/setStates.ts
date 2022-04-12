f0bf864e3a45875ee256ef32d4fd3cb86c4d7c30
Przemek <przemek.wiktorski@escola.pl>
2022-04-12 21:53:48 +0200

    generic setstate

--- src/react/context/setStates.ts
+++ src/react/context/setStates.ts
@@ -167,3 +167,80 @@ export const fetchListStateData = <T1>(
 //       }));
 //     });
 // };
+
+type fetchDataType<T> =
+  | {
+      mode: "paginated";
+      fetchAction: Promise<API.DefaultMetaResponse<T>>;
+      setState: React.Dispatch<React.SetStateAction<ContextPaginatedMetaState<T>>>;
+    }
+  | {
+      mode: "value";
+      fetchAction: Promise<API.DefaultResponse<T>>;
+      setState: React.Dispatch<React.SetStateAction<ContextStateValue<T>>>;
+    }
+  | {
+      mode: "list";
+      fetchAction: Promise<API.DefaultResponse<T>>;
+      setState: React.Dispatch<React.SetStateAction<ContextListState<T>>>;
+    };
+
+export const fetchDataType = <T>(params: fetchDataType<T>) => {
+  const { setState, fetchAction, mode } = params;
+
+  if (mode === "paginated") {
+    setState((prevState) => ({ ...prevState, loading: true }));
+  }
+  if (mode === "value") {
+    setState((prevState) => ({ ...prevState, loading: true }));
+  }
+  if (mode === "list") {
+    setState((prevState) => ({ ...prevState, loading: true }));
+  }
+
+  fetchAction
+    .then((response: unknown) => {
+      if (mode === "paginated") {
+        if ((response as API.DefaultMetaResponse<T>).success) {
+          setState({
+            loading: false,
+            list: response as API.PaginatedMetaList<T>,
+            error: undefined,
+          });
+        }
+      }
+      if (mode === "value") {
+        if ((response as API.DefaultResponse<T>).success) {
+          setState({
+            loading: false,
+            value: (response as API.DefaultResponseSuccess<T>).data,
+            error: undefined,
+          });
+        }
+      }
+      if (mode === "list") {
+        if ((response as API.DefaultResponse<T>).success) {
+          setState({
+            loading: false,
+            list: (response as API.DefaultResponseSuccess<T[]>).data,
+            error: undefined,
+          });
+        }
+      }
+
+      if ((response as API.DefaultResponseError).success === false) {
+        setState((prevState: any) => ({
+          ...prevState,
+          loading: false,
+          error: response,
+        }));
+      }
+    })
+    .catch((error) => {
+      setState((prevState: any) => ({
+        ...prevState,
+        loading: false,
+        error: error,
+      }));
+    });
+};
