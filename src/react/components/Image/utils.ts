f466a8613b7ae62f7a450faaacbf6df529e2739e
Mateusz Qunabu <mateusz@qunabu.com>
2022-04-13 15:25:22 +0200

    s3 update (#105)
    * s3 update
    
    * prettier fix
    
    Co-authored-by: Mateusz Wojczal <mateusz@wojczal.com>

--- src/react/components/Image/utils.ts
+++ src/react/components/Image/utils.ts
@@ -1,33 +1,56 @@
 import jsSHA from "jssha";
 
-const IMG_ENDPOINT = `/api/images/img`
-const CACHE_ENDPOINT = `/storage/imgcache`
+const IMG_ENDPOINT = `/api/images/img`;
+const CACHE_ENDPOINT = `/storage/imgcache`;
+
+// https://startup-academy-stage.s3.eu-central-1.amazonaws.com/imgcache/5c14861ecd894c6eccc5604658cc2401f8a82aaf.jpg
 
 const getHash = (string: string) => {
-    const sha = new jsSHA('SHA-1', "TEXT", { encoding: "UTF8" })
-    sha.update(string)
-    return sha.getHash('HEX')
-}
+  const sha = new jsSHA("SHA-1", "TEXT", { encoding: "UTF8" });
+  sha.update(string);
+  return sha.getHash("HEX");
+};
 
 const paramsToUrl = (params: object) =>
-    Object.entries(params)
-        .map((e) => typeof e[1] === 'undefined' ? '' : e.join("="))
-        .join("&");
+  Object.entries(params)
+    .map((e) => (typeof e[1] === "undefined" ? "" : e.join("=")))
+    .join("&");
 
 const getStringifyParams = (params: object) => {
-    const paramsInString = JSON.stringify(params)
-    // in backend, empty params are empty array
-    return paramsInString === JSON.stringify({}) ? '[]' : paramsInString
-}
+  const paramsInString = JSON.stringify(params);
+  // in backend, empty params are empty array
+  return paramsInString === JSON.stringify({}) ? "[]" : paramsInString;
+};
 
-export const getImageCachePath = (apiUrl: string, imagePath: string, params: Record<string, string> = {}) => {
-    const hash = getHash(imagePath + getStringifyParams(params));
-    return `${apiUrl}${CACHE_ENDPOINT}/${hash}.${imagePath.split(".").pop()}`;
-}
+export const getImagePrefix = (apiUrl: string) =>
+  /** TS ignore below is on purpose */
+  //@ts-ignore
+  process?.env?.CACHE_PREFIX ||
+  //@ts-ignore
+  process?.env?.NEXT_PUBLIC_CACHE_PREFIX ||
+  //@ts-ignore
+  process?.env?.REACT_APP_CACHE_PREFIX ||
+  //@ts-ignore
+  window?.CACHE_PREFIX ||
+  `${apiUrl}/storage/imgcache`;
 
-export const getImageApiPath = (apiUrl: string, imagePath: string, params: object = {}) => {
-    return `${apiUrl}${IMG_ENDPOINT}/?${paramsToUrl({
-        ...params,
-        path: imagePath
-    })}`;
-}
+export const getImageCachePath = (
+  apiUrl: string,
+  imagePath: string,
+  params: Record<string, string> = {}
+) => {
+  const prefix = getImagePrefix(apiUrl);
+  const hash = getHash(imagePath + getStringifyParams(params));
+  return `${prefix}/${hash}.${imagePath.split(".").pop()}`;
+};
+
+export const getImageApiPath = (
+  apiUrl: string,
+  imagePath: string,
+  params: object = {}
+) => {
+  return `${apiUrl}${IMG_ENDPOINT}/?${paramsToUrl({
+    ...params,
+    path: imagePath,
+  })}`;
+};
