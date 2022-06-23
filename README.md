98783ac786c53f9c2a45c176934f6fea413614bf
Mateusz Qunabu <mateusz@qunabu.com>
2022-06-23 16:30:51 +0200

    CI tests (#148)
    * CI tests
    
    * CI tests
    
    * CI tests
    
    * CI tests
    
    Co-authored-by: Mateusz Wojczal <mateusz@wojczal.com>

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # EscolaLMS Software Development Kit
 
+[![codecov](https://codecov.io/gh/EscolaLMS/sdk/branch/main/graph/badge.svg?token=qkNOJG7bLh)](https://codecov.io/gh/EscolaLMS/sdk)
 [![Maintainability](https://api.codeclimate.com/v1/badges/55841ab42538f51a42d2/maintainability)](https://codeclimate.com/github/EscolaLMS/sdk/maintainability)
 ![npm](https://img.shields.io/npm/v/@escolalms/sdk)
 ![npm](https://img.shields.io/npm/dm/@escolalms/sdk)
---
+++ src/react/context/index.test.tsx
@@ -0,0 +1,57 @@
+import { useContext } from "react";
+import { act } from "react-dom/test-utils";
+import { EscolaLMSContext } from "./index";
+import { render, fireEvent, waitFor, screen } from "../../../tests/test-utils";
+
+import { response as categoriesResponse } from "../../../tests/test_server/categories";
+import { response as configResponse } from "../../../tests/test_server/config";
+import { response as settingsResponse } from "../../../tests/test_server/settings";
+import { response as tagsResponse } from "../../../tests/test_server/tags";
+
+import "@testing-library/jest-dom";
+
+import fakeServer from "../../../tests/test_server";
+
+const wait = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
+
+beforeAll(() => {
+  fakeServer();
+});
+
+const InititalFetches = () => {
+  const { settings, categoryTree, config } = useContext(EscolaLMSContext);
+
+  return (
+    <div>
+      <div>{config.loading ? "Loading ..." : "Loaded"}</div>
+      <div data-testid="settings">{JSON.stringify(settings)}</div>
+      <div data-testid="categoryTree">{JSON.stringify(categoryTree)}</div>
+      <div data-testid="config">{JSON.stringify(config)}</div>
+    </div>
+  );
+};
+
+it("checks initial fetches", async () => {
+  await act(async () => {
+    render(<InititalFetches />);
+  });
+  await wait(1000); // wait for the API Response
+
+  await waitFor(() => {
+    expect(screen.queryByText("Loaded")).toBeInTheDocument();
+  });
+
+  act(() => {
+    expect(screen.getByTestId("settings")).toHaveTextContent(
+      JSON.stringify(settingsResponse.data)
+    );
+    expect(screen.getByTestId("categoryTree")).toHaveTextContent(
+      JSON.stringify(categoriesResponse.data)
+    );
+    expect(screen.getByTestId("config")).toHaveTextContent(
+      JSON.stringify(configResponse.data)
+    );
+  });
+});
+
+export {}; // 👈️ if you don't have anything else to export
