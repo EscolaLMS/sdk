98de0d5108a2def42537cce7a1c8578ccb13308a
myslaf <przemodesign@gmail.com>
2023-04-14 17:45:52 +0200

    Add useCart hook. Add useProgress hook.

---
+++ src/react/hooks/useCart.ts
@@ -0,0 +1,49 @@
+import { useContext, useEffect, useState } from "react";
+import { EscolaLMSContext } from "../context";
+import { Cart, DefaultResponseSuccess } from "../../types/api";
+
+type PogressState = {
+  data: Cart | undefined;
+  loaded: boolean;
+  loading: boolean;
+};
+
+export const useCart = () => {
+  const [cart, setCart] = useState<PogressState>({
+    data: undefined,
+    loaded: false,
+    loading: false,
+  });
+  const { user, fetchCart } = useContext(EscolaLMSContext);
+
+  useEffect(() => {
+    if (user?.value && !user.loading && !cart.loading && !cart.loaded) {
+      setCart({
+        ...cart,
+        loading: true,
+      });
+      fetchCart()
+        .then((res) => {
+          const response = res as DefaultResponseSuccess<Cart>;
+          if (response.success) {
+            setCart({
+              data: response.data,
+              loaded: true,
+              loading: false,
+            });
+          }
+        })
+        .catch(() =>
+          setCart({
+            ...cart,
+            loaded: true,
+            loading: false,
+          })
+        );
+    }
+  }, [user, cart, fetchCart]);
+
+  return {
+    cart,
+  };
+};
---
+++ src/react/hooks/useProgress.ts
@@ -0,0 +1,50 @@
+import { useContext, useEffect, useState } from "react";
+import { CourseProgress, DefaultResponseSuccess } from "../../types/api";
+import { EscolaLMSContext } from "../context";
+
+type PogressState = {
+  data: CourseProgress | undefined;
+  loaded: boolean;
+  loading: boolean;
+};
+
+export const useProgress = () => {
+  const [progress, setProgress] = useState<PogressState>({
+    data: undefined,
+    loaded: false,
+    loading: false,
+  });
+  const { user, fetchProgress } = useContext(EscolaLMSContext);
+
+  useEffect(() => {
+    if (user?.value && !user.loading && !progress.loading && !progress.loaded) {
+      setProgress({
+        ...progress,
+        loading: true,
+      });
+      fetchProgress()
+        .then((res) => {
+          const response =
+            res as DefaultResponseSuccess<CourseProgress>;
+          if (response.success) {
+            setProgress({
+              data: response.data,
+              loaded: true,
+              loading: false,
+            });
+          }
+        })
+        .catch(() =>
+          setProgress({
+            ...progress,
+            loaded: true,
+            loading: false,
+          })
+        );
+    }
+  }, [user, progress, fetchProgress]);
+
+  return {
+    progress,
+  };
+};
