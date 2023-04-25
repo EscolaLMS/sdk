d06a01da3db6f18ab2063f98d58233c43bd3256a
Szymon Dolnik <szymon2407@hotmail.com>
2023-04-25 15:30:10 +0200

    Forward ref instead of functional component

--- src/react/components/Image/index.tsx
+++ src/react/components/Image/index.tsx
@@ -1,10 +1,9 @@
 import React, {
   useContext,
-  useRef,
-  useLayoutEffect,
   useCallback,
   useMemo,
   useState,
+  forwardRef,
 } from "react";
 import { EscolaLMSContext } from "../../context";
 
@@ -34,110 +33,107 @@ const getSrcSet = (srcSetPaths: SizeObj[] = []) =>
     .map((srcSetPath) => `${getSrc(srcSetPath)} ${srcSetPath.size}w`)
     .join(",");
 
-const Image: React.FC<ImageProps> = ({
-  path,
-  size,
-  srcSizes,
-  alt = "LMS Image",
-  format = "webp",
-  ...props
-}) => {
-  const { apiUrl, getImagePrefix } = useContext(EscolaLMSContext);
+const Image = forwardRef<HTMLImageElement, ImageProps>(
+  (
+    { path, size, srcSizes, alt = "LMS Image", format = "webp", ...props },
+    imgRef
+  ) => {
+    const [cached, setCached] = useState(false);
+    const { apiUrl, getImagePrefix } = useContext(EscolaLMSContext);
 
-  const imgRef = useRef<HTMLImageElement>(null);
-  const imgSize = useMemo(
-    () => (srcSizes?.[0] ? srcSizes[0] : size),
-    [size, srcSizes]
-  ); // can be undefined
+    const imgSize = useMemo(
+      () => (srcSizes?.[0] ? srcSizes[0] : size),
+      [size, srcSizes]
+    ); // can be undefined
 
-  const getSizeObj = useCallback(
-    (size?: number, isDefault: boolean = false) => ({
-      // super important that all param values are strings
-      cachePath: getImageCachePath(
-        path,
-        size ? { w: size.toString() } : undefined,
-        getImagePrefix(),
-        format
-      ),
-      apiPath: getImageApiPath(
-        apiUrl,
-        path,
-        size ? { w: size.toString() } : undefined,
-        format
-      ),
-      size: size,
-      inCache: true,
-      isDefault,
-    }),
-    [apiUrl, path]
-  );
+    const getSizeObj = useCallback(
+      (size?: number, isDefault: boolean = false) => ({
+        // super important that all param values are strings
+        cachePath: getImageCachePath(
+          path,
+          size ? { w: size.toString() } : undefined,
+          getImagePrefix(),
+          format
+        ),
+        apiPath: getImageApiPath(
+          apiUrl,
+          path,
+          size ? { w: size.toString() } : undefined,
+          format
+        ),
+        size: size,
+        inCache: true,
+        isDefault,
+      }),
+      [apiUrl, path]
+    );
 
-  // array of all sizes
-  const [sizesPaths, setSizesPaths] = useState<SizeObj[]>([
-    getSizeObj(imgSize, true),
-    ...(srcSizes || []).map((ss) => getSizeObj(ss)),
-  ]);
+    // array of all sizes
+    const [sizesPaths, setSizesPaths] = useState<SizeObj[]>([
+      getSizeObj(imgSize, true),
+      ...(srcSizes || []).map((ss) => getSizeObj(ss)),
+    ]);
 
-  const src = useMemo<string>(
-    () => getSrc(sizesPaths.find((path) => path.isDefault)!),
-    [sizesPaths]
-  );
-  const srcSet = useMemo<string>(
-    () => getSrcSet(sizesPaths.filter((path) => !path.isDefault)),
-    [sizesPaths]
-  );
+    const src = useMemo<string>(
+      () => getSrc(sizesPaths.find((path) => path.isDefault)!),
+      [sizesPaths]
+    );
+    const srcSet = useMemo<string>(
+      () => getSrcSet(sizesPaths.filter((path) => !path.isDefault)),
+      [sizesPaths]
+    );
 
-  useLayoutEffect(() => {
-    if (imgRef.current !== null) {
-      imgRef.current.onerror = (error) => {
-        if (imgRef.current !== null) {
-          const currentSrc = imgRef.current.currentSrc;
-          imgRef.current.onerror = null;
+    const onError = useCallback(
+      (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+        const currentSrc = e.currentTarget.currentSrc;
+        setCached(true);
 
-          // if img is not cached yet, then use generator url
-          if (sizesPaths.some((sp) => sp.cachePath === currentSrc)) {
-            setSizesPaths(
-              sizesPaths.map((sizeObj) => {
-                if (sizeObj.cachePath !== currentSrc) return sizeObj;
+        // if img is not cached yet, then use generator url
+        if (sizesPaths.some((sp) => sp.cachePath === currentSrc)) {
+          setSizesPaths(
+            sizesPaths.map((sizeObj) => {
+              if (sizeObj.cachePath !== currentSrc) return sizeObj;
 
-                return {
-                  ...sizeObj,
-                  inCache: false,
-                };
-              })
-            );
-          }
+              return {
+                ...sizeObj,
+                inCache: false,
+              };
+            })
+          );
         }
-      };
-    }
-  }, []);
+      },
+      [sizesPaths]
+    );
 
-  if (path.includes(".svg")) {
+    if (path.includes(".svg")) {
+      return (
+        <div className="escolalms-image">
+          <img
+            loading={"lazy"}
+            ref={imgRef}
+            onError={cached ? undefined : onError}
+            src={`${getImagePrefix()}${path}}`}
+            alt={alt}
+            {...props}
+          />
+        </div>
+      );
+    }
+
     return (
       <div className="escolalms-image">
         <img
           loading={"lazy"}
           ref={imgRef}
-          src={`${getImagePrefix()}${path}}`}
+          onError={cached ? undefined : onError}
+          src={src}
+          srcSet={srcSet !== "" ? srcSet : undefined}
           alt={alt}
           {...props}
         />
       </div>
     );
   }
-
-  return (
-    <div className="escolalms-image">
-      <img
-        loading={"lazy"}
-        ref={imgRef}
-        src={src}
-        srcSet={srcSet !== "" ? srcSet : undefined}
-        alt={alt}
-        {...props}
-      />
-    </div>
-  );
-};
+);
 
 export default Image;
