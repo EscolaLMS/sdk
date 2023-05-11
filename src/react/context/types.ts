59421928ce7c9deed8ba5539e5532201a906e8a5
Piotr Rudzki <piotr.rudzki@escolasoft.com>
2023-05-11 09:08:47 +0200

    Fix AddProductResponse type

--- src/react/context/types.ts
+++ src/react/context/types.ts
@@ -138,10 +138,7 @@ export interface EscolaLMSContextAPIConfig {
   forgot: (body: API.ForgotRequest) => Promise<API.AuthResponse>;
   reset: (body: API.ResetPasswordRequest) => Promise<API.AuthResponse>;
   emailVerify: (id: string, hash: string) => Promise<API.AuthResponse>;
-  addToCart: (
-    id: number,
-    quantity?: number
-  ) => Promise<API.DefaultResponse<API.AddProductResponse>>;
+  addToCart: (id: number, quantity?: number) => Promise<API.AddProductResponse>;
   addMissingProducts: (products: number[]) => Promise<void>;
   removeFromCart: (courseId: number) => Promise<void>;
   fetchCart: () => Promise<
