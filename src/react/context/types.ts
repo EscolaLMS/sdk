539d523a9c9cb4eb1ddcde48ffe50705e96330ae
Mateusz Motyka <mateusz.motyka@escolasoft.com>
2023-08-11 09:27:04 +0200

    cr fix

--- src/react/context/types.ts
+++ src/react/context/types.ts
@@ -174,11 +174,7 @@ export interface EscolaLMSContextAPIConfig {
     | API.DefaultResponse<API.CourseProgress>
     | API.DefaultMetaResponse<API.CourseProgress>
   >;
-  fetchMyAuthoredCourses: () => Promise<
-    | void
-    | API.DefaultResponse<API.Course[]>
-    | API.DefaultMetaResponse<API.Course[]>
-  >;
+  fetchMyAuthoredCourses: () => Promise<API.DefaultResponse<API.Course[]>>;
   fetchCourseProgress: (courseId: number) => Promise<void>;
   sendProgress: (
     courseId: number,
