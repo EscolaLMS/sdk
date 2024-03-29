bdb28e921e8d705614965f4eedf53afe54e522af
myslaf <przemodesign@gmail.com>
2024-03-29 14:45:04 +0100

    Add missing types. Add token to dictionary word request.

--- src/react/context/dictionary/dictionariesWord.tsx
+++ src/react/context/dictionary/dictionariesWord.tsx
@@ -3,25 +3,27 @@ import {
   FunctionComponent,
   PropsWithChildren,
   useCallback,
+  useContext,
   useEffect,
   useRef,
-} from "react";
+} from 'react';
 import {
   EscolaLMSContextConfig,
   EscolaLMSContextReadConfig,
   ContextStateValue,
-} from "../types";
-import { defaultConfig } from "../defaults";
-import { fetchDataType } from "../states";
+} from '../types';
+import { defaultConfig } from '../defaults';
+import { fetchDataType } from '../states';
 
-import { useLocalStorage } from "../../hooks/useLocalStorage";
-import * as API from "../../../types/api";
-import { getDefaultData } from "../index";
+import { useLocalStorage } from '../../hooks/useLocalStorage';
+import * as API from '../../../types/api';
+import { getDefaultData } from '../index';
 
-import { dictionariesWord as getDictionariesWord } from "../../../services/dictionary";
+import { dictionariesWord as getDictionariesWord } from '../../../services/dictionary';
+import { UserContext } from '../user';
 
 export const DictionariesWordContext: React.Context<
-  Pick<EscolaLMSContextConfig, "dictionariesWord" | "fetchDictionariesWord">
+  Pick<EscolaLMSContextConfig, 'dictionariesWord' | 'fetchDictionariesWord'>
 > = createContext({
   dictionariesWord: defaultConfig.dictionariesWord,
   fetchDictionariesWord: defaultConfig.fetchDictionariesWord,
@@ -29,7 +31,7 @@ export const DictionariesWordContext: React.Context<
 
 export interface DictionariesWordContextProviderType {
   apiUrl: string;
-  defaults?: Partial<Pick<EscolaLMSContextReadConfig, "dictionariesWord">>;
+  defaults?: Partial<Pick<EscolaLMSContextReadConfig, 'dictionariesWord'>>;
   ssrHydration?: boolean;
 }
 
@@ -37,13 +39,14 @@ export const DictionariesWordContextProvider: FunctionComponent<
   PropsWithChildren<DictionariesWordContextProviderType>
 > = ({ children, defaults, apiUrl, ssrHydration }) => {
   const abortControllers = useRef<Record<string, AbortController | null>>({});
+  const { token } = useContext(UserContext);
 
   const [dictionariesWord, setDictionariesWord] = useLocalStorage<
     ContextStateValue<API.DictionariesWords>
   >(
-    "lms",
-    "dictionariesWord",
-    getDefaultData("dictionariesWord", {
+    'lms',
+    'dictionariesWord',
+    getDefaultData('dictionariesWord', {
       ...defaultConfig,
       ...defaults,
     }),
@@ -66,13 +69,16 @@ export const DictionariesWordContextProvider: FunctionComponent<
       controllers: abortControllers.current,
       controller: `dictionariesWord${id}`,
       id: id,
-      mode: "value",
+      mode: 'value',
       fetchAction: getDictionariesWord.bind(null, apiUrl)(slug, id, {
         signal: abortControllers.current?.[`dictionariesWord${id}`]?.signal,
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
       }),
       setState: setDictionariesWord,
     });
-  }, []);
+  }, [token]);
 
   return (
     <DictionariesWordContext.Provider
